40be0e82f76325381f5970bb731aed28c1b3c630
Andras Retzler <andras.retzler@lombiq.com>
2018-01-28 15:05:42 +0100

    Fixed use and library statements in AdditionalVhdlIncludes
    --HG--
    branch : issue/HAST-133

--- AdditionalVhdlIncludes.cs
+++ AdditionalVhdlIncludes.cs
@@ -34,7 +34,7 @@ package body TypeConversion is
 
 end TypeConversion;
 
-library work;
+library ieee;use ieee.std_logic_1164.all;use ieee.numeric_std.all;library work;
 use work.TypeConversion.all;
 
 ";
