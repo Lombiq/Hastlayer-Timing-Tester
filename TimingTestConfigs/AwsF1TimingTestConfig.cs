381aeb13a6c61fad2dc637bae321089fa7953c09
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2020-03-05 20:20:58 +0100

    More precise not on AWS FPGA timing tester RAM usage
    --HG--
    branch : dev

--- TimingTestConfigs/AwsF1TimingTestConfig.cs
+++ TimingTestConfigs/AwsF1TimingTestConfig.cs
@@ -19,7 +19,8 @@
             Name = "AWSF1";
             Part = "xcvu9p-flgb2104-2-i";
             Frequency = 250e6m;
-            // A process will use up to about 4,5 GB of RAM so more won't fit on a 32 GB RAM VM.
+            // A process will usually use up to about 4,5 GB of RAM (with one-two processes bursting to 5 GB) so more
+            // won't fit on a 32 GB RAM VM.
             NumberOfSTAProcesses = 6;
         }
     }
